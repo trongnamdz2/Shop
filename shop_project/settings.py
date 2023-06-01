cb8196fb17ae927b0404ca7be2fcd15e3b6f3420
Trong Nam Nguyen <nguyencdf@gmail.com>
2023-06-01 10:40:21 -0500

    second commit

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'shop_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'shop_project',
+        'USER': 'root',
+        'PASSWORD': 'bin1237894588',
+        'HOST': 'localhost',
+        'PORT': '3305',
     }
 }
 
